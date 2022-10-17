b6ff931eb0cd27099cd388789ddd1baab272e8d0
Sheldon Soong <sheldon.soong@live.ca>
2022-10-17 01:15:03 -0400

    Started CRUD functions for User.

---
+++ src/main/java/backend/CRUD_Appointment.java
@@ -0,0 +1,11 @@
+package backend;
+
+/**
+ *
+ * @author Connor
+ */
+public class CRUD_Appointment {
+    // make database connection
+    // create, read, update, delete appointment data in the database
+
+}

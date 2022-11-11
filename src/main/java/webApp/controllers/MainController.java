8f1b49fdd16d11182fd76b43105c3ca73d3860d9
Connor-Virgin <connorv673@gmail.com>
2022-11-11 02:21:38 -0500

    Added  SessionUser

--- src/main/java/webApp/controllers/MainController.java
+++ src/main/java/webApp/controllers/MainController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import webApp.models.SessionUser;
+
 @Controller
 public class MainController {
 
      //Deals with the HTML get request '/main'
      @GetMapping("/main")
-     public String main(Model model) {
+     public String main(Model model, SessionUser sessionUser) {
          //Thymeleaf looks for main.html in resources/templates/
          return "main";
      }
--- src/main/java/webApp/services/LoginService.java
+++ src/main/java/webApp/services/LoginService.java
@@ -2,6 +2,7 @@ package webApp.services;
 
 import org.springframework.stereotype.Service;
 import webApp.models.Login;
+import webApp.models.SessionUser;
 import backend.DB_User;
 import objects.User;
 
@@ -26,4 +27,24 @@ public class LoginService {
             return false;
         }
     }
+
+    //Calls to DB_User to form SessionUser from login information, returns null if user not found in DB
+    public SessionUser loginUser(Login login){
+        try{
+            User userInfo = DB_User.getUserLogin(login.getEmail(), login.getPassword());
+            if(userInfo == null){
+                return null;
+            }
+            else{
+                SessionUser sessionUser = new SessionUser(userInfo.getFirstName(), userInfo.getLastName(),
+                 userInfo.getEmail(),userInfo.getRole(), userInfo.getUserId());
+                return sessionUser;
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
 }

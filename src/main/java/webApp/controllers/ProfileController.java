ce91c82ea4215f007b1645a0e7dc3532fd5eb1a6
Connor-Virgin <connorv673@gmail.com>
2022-11-17 00:47:52 -0500

    Navigation to profile page

--- src/main/java/webApp/controllers/ProfileController.java
+++ src/main/java/webApp/controllers/ProfileController.java
@@ -3,16 +3,26 @@ package webApp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import webApp.models.Appt;
 import webApp.models.SessionUser;
 
 @Controller
+@SessionAttributes({"sessionUser"})
 public class ProfileController {
+
+    //Used to keep sessionUser info on page even when reloaded
+    @ModelAttribute("sessionUser")
+    public SessionUser sessionUser(){
+        return new SessionUser();
+    }
 
      //Deals with the HTML get request '/profile'
      @GetMapping("/profile")
-     public String profile(Model model, SessionUser sessionUser) {
+     public String profile(Model model, SessionUser sessionUser,  Appt appt) {
          //Thymeleaf looks for profile.html in resources/templates/
          return "profile";
      }
